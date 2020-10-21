34f23a7e5d0f708ff102d7f0a40fd0e010cb3c8b
Fagonar96 <fagonar96@gmail.com>
2020-10-21 15:54:27 -0600

    Menu v2

--- menu.c
+++ menu.c
@@ -9,8 +9,12 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define SCREENWIDTH 630
-#define SCREENHEIGHT 680
+#define SCREENWIDTH 690
+#define SCREENHEIGHT 820
+
+void draw_menu(ALLEGRO_FONT *title, ALLEGRO_FONT *mode, ALLEGRO_BITMAP *stars);
+void draw_manual(ALLEGRO_FONT *subtitle, ALLEGRO_FONT *stat, ALLEGRO_FONT *number, int energy_alien, int regen_alien);
+void draw_autom(ALLEGRO_FONT *subtitle, ALLEGRO_FONT *stat, ALLEGRO_FONT *number, ALLEGRO_BITMAP *stars, int num_alien, int energy_alien, int regen_alien);
 
 int main()
 {   
@@ -36,33 +40,29 @@ int main()
     al_install_keyboard();
     al_init_image_addon();
 
-    ALLEGRO_EVENT_QUEUE *event_menu = al_create_event_queue();
-    al_register_event_source(event_menu, al_get_keyboard_event_source());
-
     ALLEGRO_FONT *title = al_load_ttf_font("Orbitron-Bold.ttf", 36, ALLEGRO_TTF_MONOCHROME);
     ALLEGRO_FONT *mode = al_load_ttf_font("Orbitron-Bold.ttf", 28, ALLEGRO_TTF_MONOCHROME);
     ALLEGRO_BITMAP *stars = al_load_bitmap("stars.jpg");
+
+    ALLEGRO_EVENT_QUEUE *event_menu = al_create_event_queue();
+    al_register_event_source(event_menu, al_get_keyboard_event_source());
 
     int modeop;
 
     bool menu = false;
     while(!menu)
     {
-        al_draw_bitmap(stars, 0, 0, 0);
-        al_draw_text(title, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, "ALIEN");
-        al_draw_text(mode, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, SCREENHEIGHT / 2, ALLEGRO_ALIGN_CENTRE, "1) Modo Manual");
-        al_draw_text(mode, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, 3 * SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, "2) Modo Automatico");
-        al_flip_display();
+        
+        draw_menu(title, mode, stars);
 
         ALLEGRO_EVENT events1;
         al_wait_for_event(event_menu, &events1);
-
         if (events1.type == ALLEGRO_EVENT_KEY_DOWN)
         {
             switch (events1.keyboard.keycode)
             {
             case ALLEGRO_KEY_X:
-                menu = true;
+                exit(1);
                 break;
             case ALLEGRO_KEY_1:
                 modeop = 1;
@@ -85,22 +85,75 @@ int main()
 
     //printf("%d\n", modeop);
     if(modeop == 1)
-    {
+    {   
+        ALLEGRO_FONT *subtitle = al_load_ttf_font("Orbitron-Bold.ttf", 22, ALLEGRO_TTF_MONOCHROME);
+        ALLEGRO_FONT *stat = al_load_ttf_font("Orbitron-Bold.ttf", 16, ALLEGRO_TTF_MONOCHROME);
+        ALLEGRO_FONT *number =  al_load_ttf_font("Orbitron-Bold.ttf", 20, ALLEGRO_TTF_MONOCHROME);
+
+        int energy_alien = 1;
+        int regen_alien = 1;
+
+        ALLEGRO_EVENT_QUEUE *event_manual = al_create_event_queue();
+        al_register_event_source(event_manual, al_get_keyboard_event_source());
+
         bool manual = false;
+        while(!manual)
+        {
+            draw_manual(subtitle, stat, number, energy_alien, regen_alien);
+
+            ALLEGRO_EVENT events2;
+            al_wait_for_event(event_manual, &events2);
+
+            if (events2.type == ALLEGRO_EVENT_KEY_DOWN)
+            {
+                switch (events2.keyboard.keycode)
+                {
+                case ALLEGRO_KEY_X:
+                    manual = true;
+                    break;
+                case ALLEGRO_KEY_E:
+                    energy_alien += 1;
+                    break;
+                case ALLEGRO_KEY_D:
+                    if (energy_alien > 1)
+                    {
+                        energy_alien -= 1;
+                    }
+                    break;
+                case ALLEGRO_KEY_R:
+                    regen_alien += 1;
+                    break;
+                case ALLEGRO_KEY_F:
+                    if(regen_alien > 1)
+                    {
+                        regen_alien -= 1;
+                    }
+                    break;
+                case ALLEGRO_KEY_ENTER:
+                    //funcion de crear marciano
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            al_clear_to_color(al_map_rgb(0,0,0));
+        }
+        al_destroy_font(subtitle);
+        al_destroy_font(number);
+        al_destroy_font(stat);
+        al_destroy_event_queue(event_manual);
 
     }
     else if(modeop == 2)
     {
-        ALLEGRO_FONT *number =  al_load_ttf_font("Orbitron-Bold.ttf", 36, ALLEGRO_TTF_MONOCHROME);
         ALLEGRO_FONT *subtitle = al_load_ttf_font("Orbitron-Bold.ttf", 36, ALLEGRO_TTF_MONOCHROME);
-        int num_alien = 1;
-        char alien_num[10];
-
         ALLEGRO_FONT *stat = al_load_ttf_font("Orbitron-Bold.ttf", 16, ALLEGRO_TTF_MONOCHROME);
+        ALLEGRO_FONT *number =  al_load_ttf_font("Orbitron-Bold.ttf", 36, ALLEGRO_TTF_MONOCHROME);
+
+        int num_alien = 1;
         int energy_alien = 1;
-        char alien_energy[10];
         int regen_alien = 1;
-        char alien_regen[10];
 
         ALLEGRO_EVENT_QUEUE *event_autom = al_create_event_queue();
         al_register_event_source(event_autom, al_get_keyboard_event_source());
@@ -108,40 +161,14 @@ int main()
         bool autom = false;
         while(!autom)
         {   
-            al_draw_bitmap(stars, 0, 0, 0);
+            draw_autom(subtitle, stat, number, stars, num_alien, energy_alien, regen_alien);
 
-            char alien[256] = "CREAR MARCIANO #";
-            sprintf(alien_num, "%d", num_alien);
-            strcat(alien, alien_num);
-            al_draw_text(subtitle, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, 10, ALLEGRO_ALIGN_CENTRE, alien);
+            ALLEGRO_EVENT events3;
+            al_wait_for_event(event_autom, &events3);
 
-            char energy[256] = "PRESIONE 'E' PARA AUMENTAR LA ENERGIA";
-            char denergy[256] = "PRESIONE 'D' PARA DISMINUIR LA ENERGIA";
-            sprintf(alien_energy, "%d", energy_alien);
-            al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, energy);
-            al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 4) + 20, ALLEGRO_ALIGN_CENTRE, denergy);
-            al_draw_text(number, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 4) + 60, ALLEGRO_ALIGN_CENTRE, alien_energy);
-
-
-            char regen[256] = "PRESIONE 'R' PARA AUMENTAR TIEMPO DE REGENERACION";
-            char dregen[256] = "PRESIONE 'F' PARA DISMINUIR EL TIEMPO DE REGENERACION";
-            sprintf(alien_regen, "%d", regen_alien);
-            al_draw_text(stat, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, SCREENHEIGHT / 2, ALLEGRO_ALIGN_CENTRE, regen);
-            al_draw_text(stat, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 2) + 20, ALLEGRO_ALIGN_CENTRE, dregen);
-            al_draw_text(number, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 2) + 60, ALLEGRO_ALIGN_CENTRE, alien_regen);
-
-            char next[256] = "SIGUIENTE MARCIANO: 'ESPACIO'";
-            char start[256] = "INICIAR SIMULACION: 'ENTER'";
-            al_draw_text(stat, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, (3 * SCREENHEIGHT / 4) + 20, ALLEGRO_ALIGN_CENTRE, next);
-            al_draw_text(stat, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, (3 * SCREENHEIGHT / 4) + 40, ALLEGRO_ALIGN_CENTRE, start);
-            al_flip_display();
-
-            ALLEGRO_EVENT events2;
-            al_wait_for_event(event_autom, &events2);
-
-            if (events2.type == ALLEGRO_EVENT_KEY_DOWN)
+            if (events3.type == ALLEGRO_EVENT_KEY_DOWN)
             {
-                switch (events2.keyboard.keycode)
+                switch (events3.keyboard.keycode)
                 {
                 case ALLEGRO_KEY_X:
                     exit(1);
@@ -185,9 +212,70 @@ int main()
         al_destroy_font(stat);
         al_destroy_bitmap(stars);
         al_destroy_event_queue(event_autom);
+
+
+        //While del juego
     }
 
 
     al_destroy_display(display);
 
+}
+
+void draw_menu(ALLEGRO_FONT *title, ALLEGRO_FONT *mode, ALLEGRO_BITMAP *stars)
+{
+    al_draw_bitmap(stars, 0, 0, 0);
+    al_draw_text(title, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, "ALIEN");
+    al_draw_text(mode, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, SCREENHEIGHT / 2, ALLEGRO_ALIGN_CENTRE, "1) Modo Manual");
+    al_draw_text(mode, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, 3 * SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, "2) Modo Automatico");
+    al_flip_display();
+}
+
+void draw_manual(ALLEGRO_FONT *subtitle, ALLEGRO_FONT *stat, ALLEGRO_FONT *number, int energy_alien, int regen_alien)
+{
+    al_draw_text(subtitle, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, SCREENHEIGHT - 125, ALLEGRO_ALIGN_CENTRE, "CREAR MARCIANO");
+
+    char alien_energy[10];
+    sprintf(alien_energy, "%d", energy_alien);
+    al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 4, SCREENHEIGHT - 90, ALLEGRO_ALIGN_CENTRE, "ENERGIA");
+    al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 4, SCREENHEIGHT - 70, ALLEGRO_ALIGN_CENTRE, "'E': AUMENTAR");
+    al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 4, SCREENHEIGHT - 50, ALLEGRO_ALIGN_CENTRE, "'D': DISMINUIR");
+    al_draw_text(number, al_map_rgb(0, 255, 0), SCREENWIDTH / 4, SCREENHEIGHT - 30, ALLEGRO_ALIGN_CENTRE, alien_energy);
+
+    char alien_regen[10];
+    sprintf(alien_regen, "%d", regen_alien);
+    al_draw_text(stat, al_map_rgb(255, 128, 0), 3 * SCREENWIDTH / 4, SCREENHEIGHT - 90, ALLEGRO_ALIGN_CENTRE, "TIEMPO DE REGENERACION");
+    al_draw_text(stat, al_map_rgb(255, 128, 0), 3 * SCREENWIDTH / 4, SCREENHEIGHT - 70, ALLEGRO_ALIGN_CENTRE, "'R': AUMENTAR");
+    al_draw_text(stat, al_map_rgb(255, 128, 0), 3 * SCREENWIDTH / 4, SCREENHEIGHT - 50, ALLEGRO_ALIGN_CENTRE, "'F': DISMINUIR");
+    al_draw_text(number, al_map_rgb(255, 128, 0), 3 * SCREENWIDTH / 4, SCREENHEIGHT - 30, ALLEGRO_ALIGN_CENTRE, alien_regen);
+
+    al_draw_text(stat, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, SCREENHEIGHT - 20, ALLEGRO_ALIGN_CENTRE, "'ENTER': CREAR");
+    al_flip_display();
+}
+
+void draw_autom(ALLEGRO_FONT *subtitle, ALLEGRO_FONT *stat, ALLEGRO_FONT *number, ALLEGRO_BITMAP *stars, int num_alien, int energy_alien, int regen_alien)
+{
+    al_draw_bitmap(stars, 0, 0, 0);
+
+    char alien_num[10];
+    char alien[256] = "CREAR MARCIANO #";
+    sprintf(alien_num, "%d", num_alien);
+    strcat(alien, alien_num);
+    al_draw_text(subtitle, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, 10, ALLEGRO_ALIGN_CENTRE, alien);
+
+    char alien_energy[10];
+    sprintf(alien_energy, "%d", energy_alien);
+    al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, SCREENHEIGHT / 4, ALLEGRO_ALIGN_CENTRE, "PRESIONE 'E' PARA AUMENTAR LA ENERGIA");
+    al_draw_text(stat, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 4) + 20, ALLEGRO_ALIGN_CENTRE, "PRESIONE 'D' PARA DISMINUIR LA ENERGIA");
+    al_draw_text(number, al_map_rgb(0, 255, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 4) + 60, ALLEGRO_ALIGN_CENTRE, alien_energy);
+
+    char alien_regen[10];
+    sprintf(alien_regen, "%d", regen_alien);
+    al_draw_text(stat, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, SCREENHEIGHT / 2, ALLEGRO_ALIGN_CENTRE, "PRESIONE 'R' PARA AUMENTAR TIEMPO DE REGENERACION");
+    al_draw_text(stat, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 2) + 20, ALLEGRO_ALIGN_CENTRE, "PRESIONE 'F' PARA DISMINUIR EL TIEMPO DE REGENERACION");
+    al_draw_text(number, al_map_rgb(255, 128, 0), SCREENWIDTH / 2, (SCREENHEIGHT / 2) + 60, ALLEGRO_ALIGN_CENTRE, alien_regen);
+
+    al_draw_text(stat, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, (3 * SCREENHEIGHT / 4) + 20, ALLEGRO_ALIGN_CENTRE, "SIGUIENTE MARCIANO: 'ESPACIO'");
+    al_draw_text(stat, al_map_rgb(44, 117, 255), SCREENWIDTH / 2, (3 * SCREENHEIGHT / 4) + 40, ALLEGRO_ALIGN_CENTRE, "INICIAR SIMULACION: 'ENTER'");
+    al_flip_display();
 }
